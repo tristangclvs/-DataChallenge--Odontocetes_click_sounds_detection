2d6641da1e51b19db77960dea41f1942b1fc6ffe
tristangclvs <goncalvestristan.pro@pm.me>
2023-12-28 14:54:34 +0100

    clean scripts

--- CNN topic/Convolution_1D/simple_testing.py
+++ CNN topic/Convolution_1D/simple_testing.py
@@ -4,34 +4,33 @@ import numpy as np
 import librosa
 from tqdm import tqdm
 from pathlib import Path
-
-def load_and_preprocess_data(file_paths, target_length): 
-    data = []
-    for file_path in tqdm(file_paths):
-        audio, _ = librosa.load(file_path, sr=None)
-        if len(audio) < target_length:
-            audio = np.pad(audio, (0, target_length - len(audio)))
-        else : 
-            audio = audio[:target_length]
-        data.append(audio)
-    return np.array(data)
+from simple_training import load_and_preprocess_data
 
 def load_test_data(folder_path, target_length):
     file_paths = list(Path(folder_path).rglob('*.wav'))  # Assuming the audio files are in WAV format
     return load_and_preprocess_data(file_paths, target_length)
 
-
 target_length = int(0.2 * 256000)
 
-X_test = load_test_data("../../.dataset/X_test", target_length)
+conv1D_directory = Path.cwd() / "CNN topic" / "Convolution_1D"
+test_directory = Path.cwd() / ".dataset" / "X_test"
+models_directory = Path.cwd() / "CNN topic" / "Convolution_1D" / "models"
+model_name = "1d_cnn_l2.keras"
 
-model = models.load_model('../models/1d_cnn.h5')
+X_test = load_test_data(test_directory, target_length)
+
+model = models.load_model(models_directory / model_name)
 
 # predictions = model.predict(X_test)
 # binary_predictions = (predictions > 0.5).astype(int)
 # print(binary_predictions.shape)
-file_names = [file_path.name for file_path in Path("../../.dataset/X_test").rglob('*.wav')]
+file_names = [file_path.name for file_path in Path(test_directory).rglob('*.wav')]
 predictions = model.predict(X_test)
 
+print("----------------------------------")
+print(predictions.shape)
+print(len(file_names))
+print("----------------------------------")
+
 df = pd.DataFrame({'id': file_names, 'pos_label': predictions[:, 0]})
-df.to_csv('submission.csv', index=False)
+df.to_csv(f"{conv1D_directory}/submission.csv", index=False)
--- CNN topic/Convolution_1D/simple_training.py
+++ CNN topic/Convolution_1D/simple_training.py
@@ -19,93 +19,124 @@ import IPython.display as ipd
 import matplotlib.pyplot as plt
 import tensorflow as tf
 from tensorflow.keras import layers, models
-
-sample_rate = 256000
-audio_duration_seconds = 0.2
-
-# Set the path to the downloaded data
-download_path = Path.cwd().parent / "../" / ".dataset"
-
-# Read labels file
-labels_file = download_path / "Y_train_ofTdMHi.csv"
-df = pd.read_csv(labels_file)
-
-# Construct file path by concatenating folder and file name
-df["relative_path"] = str(download_path) + "/X_train/" + df["id"]
-
-# Drop id column (replaced it with relative_path)
-df.drop(columns=["id"], inplace=True)
-
-df.rename(columns={"pos_label": "label"}, inplace=True)
-
-# invert relative_path and label columns positions
-df = df[["relative_path", "label"]]
-print(f"### There are **_{len(df)}_** audio files in the dataset.")
-
-table = f"""
-Here is the split into good and bad signals:
-| Label   | Count   |
-|:-------:|:-------:|
-| 0       | {df['label'].value_counts()[0]} |
-| 1       | {df['label'].value_counts()[1]} |"""
-print(table)
-
-## TODO : add a band pass filter to remove noise
+from time import time
 
 def load_and_preprocess_data(file_paths, target_length): 
     data = []
-    for file_path in tqdm(file_paths):
-        audio, _ = librosa.load(file_path, sr=None)
+    for file_path in tqdm(file_paths, desc="Loading and preprocessing data", unit="file"):
+        audio, sr = librosa.load(file_path, sr=None)
         if len(audio) < target_length:
             audio = np.pad(audio, (0, target_length - len(audio)))
-        else : 
+        else: 
             audio = audio[:target_length]
+
+        sos = signal.butter(6, [5000, 100000], 'bandpass', fs=sr, output='sos')
+        audio = signal.sosfiltfilt(sos, audio)
+
         data.append(audio)
+
+    print("Done")
     return np.array(data)
 
-target_length = int(sample_rate * audio_duration_seconds)
-X = load_and_preprocess_data(df["relative_path"], target_length)
-y = df["label"].values.astype(int)
+def build_model():
+    print("\nCreating model")
+    model = models.Sequential()
+    model.add(layers.Conv1D(32, kernel_size=9, activation='relu', input_shape=(target_length, 1)))
+    model.add(layers.MaxPooling1D(pool_size=2))
+    # Second convolutional layer
+    model.add(layers.Conv1D(32, kernel_size=9, activation='relu'))
+    model.add(layers.MaxPooling1D(pool_size=2))
+    # Third convolutional layer
+    model.add(layers.Conv1D(32, kernel_size=5, activation='relu'))
+    model.add(layers.MaxPooling1D(pool_size=2))
+    # Flatten the output for the fully connected layers
+    model.add(layers.Flatten())
+    # First fully connected layer
+    model.add(layers.Dense(128, activation='relu', kernel_regularizer='l2')) #! L2 regularization to remove after
+    # Second fully connected layer
+    model.add(layers.Dense(64, activation='relu', kernel_regularizer='l2')) #! L2 regularization to remove after
+    # Dropout regularization to avoid overfitting
+    model.add(layers.Dropout(0.5))
+    # Binary classification output layer
+    model.add(layers.Dense(1, activation='sigmoid'))
+    # Compile the model
+    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+    # Display the model summary
+    model.summary()
+    return model
 
-X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2, random_state=42)
+def plot_accuracy():
+    acc = history.history['accuracy']
+    val_acc = history.history['val_accuracy']
+    epochs = range(1, len(acc) + 1)
 
-model = models.Sequential()
-model.add(layers.Conv1D(32, kernel_size=9, activation='relu', input_shape=(target_length, 1)))
-model.add(layers.MaxPooling1D(pool_size=2))
-# Second convolutional layer
-model.add(layers.Conv1D(32, kernel_size=9, activation='relu'))
-model.add(layers.MaxPooling1D(pool_size=2))
-# Third convolutional layer
-model.add(layers.Conv1D(32, kernel_size=5, activation='relu'))
-model.add(layers.MaxPooling1D(pool_size=2))
-# Flatten the output for the fully connected layers
-model.add(layers.Flatten())
-# First fully connected layer
-model.add(layers.Dense(128, activation='relu', kernel_regularizer='l2')) #! L2 regularization to remove after
-# Second fully connected layer
-model.add(layers.Dense(64, activation='relu', kernel_regularizer='l2')) #! L2 regularization to remove after
-# Dropout regularization to avoid overfitting
-model.add(layers.Dropout(0.5))
-# Binary classification output layer
-model.add(layers.Dense(1, activation='sigmoid'))
-# Compile the model
-model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
-# Display the model summary
-model.summary()
+    plt.plot(epochs, acc, '-', label='Training Accuracy')
+    plt.plot(epochs, val_acc, ':', label='Validation Accuracy')
+    plt.title('Training and Validation Accuracy')
+    plt.xlabel('Epoch')
+    plt.ylabel('Accuracy')
+    plt.legend(loc='lower right')
+    plt.plot()
 
-history = model.fit(X_train, y_train, epochs=10, batch_size=32, validation_data=(X_val, y_val))
 
-model.save("../models/1d_cnn_l2.h5")
+# main
+if __name__ == "__main__":
 
-acc = history.history['accuracy']
-val_acc = history.history['val_accuracy']
-epochs = range(1, len(acc) + 1)
+    # ====== Set parameters ======
+    conv1D_directory = Path.cwd() / "CNN topic" / "Convolution_1D"
+    test_directory = Path.cwd() / ".dataset" / "X_test"
+    models_directory = Path.cwd() / "CNN topic" / "Convolution_1D" / "models"
+    model_name = "1d_cnn_l2.keras"
 
-plt.plot(epochs, acc, '-', label='Training Accuracy')
-plt.plot(epochs, val_acc, ':', label='Validation Accuracy')
-plt.title('Training and Validation Accuracy')
-plt.xlabel('Epoch')
-plt.ylabel('Accuracy')
-plt.legend(loc='lower right')
-plt.plot()
+    # Set the path to the downloaded data
+    download_path = Path.cwd() / ".dataset"
+
+    # Audio parameters
+    sample_rate = 256000
+    audio_duration_seconds = 0.2
+
+    #! ====== Load and preprocess data ====== 
+    # Read labels file
+    labels_file = download_path / "Y_train_ofTdMHi.csv"
+    df = pd.read_csv(labels_file)
+
+    # Construct file path by concatenating folder and file name
+    df["relative_path"] = Path(download_path) / "X_train" / df["id"]
+    # df["relative_path"] = str(download_path) + "/X_train/" + df["id"]
+
+    # Drop id column (replaced it with relative_path)
+    df.drop(columns=["id"], inplace=True)
+
+    df.rename(columns={"pos_label": "label"}, inplace=True)
+
+    # invert relative_path and label columns positions
+    df = df[["relative_path", "label"]]
+    print(f"### There are {len(df)} audio files in the dataset.")
+
+    table = f"""
+    Here is the split into good and bad signals:
+    | Label   | Count   |
+    |:-------:|:-------:|
+    | 0       | {df['label'].value_counts()[0]:.9f} |
+    | 1       | {df['label'].value_counts()[1]:.9f} |"""
+    print(table, end="\n\n")
+
+    print("Loading and preprocessing data")
+    target_length = int(sample_rate * audio_duration_seconds)
+    X = load_and_preprocess_data(df["relative_path"], target_length)
+    y = df["label"].values.astype(int)
+
+    print("\nSplitting data into train and validation sets")
+    X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    early_stopping = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=3, restore_best_weights=True)
+
+    model = build_model() # Build model
+
+    print("\n------------------ Training model ------------------", end="\n\n")
+    history = model.fit(X_train, y_train, epochs=10, batch_size=32, validation_data=(X_val, y_val), callbacks=[early_stopping])
+
+    print("\n------------------ Saving model ------------------", end="\n\n")
+    os.mkdir(Path(models_directory)) if not os.path.exists(Path(models_directory)) else None
+    model.save(Path(models_directory) / model_name)
 
