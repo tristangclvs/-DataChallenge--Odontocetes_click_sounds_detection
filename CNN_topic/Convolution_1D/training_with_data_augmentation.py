542bb62ca7b8f7dd4e83469569bcd795b0603f46
Thomas Chimbault <tchimbault@ensc.fr>
2024-01-27 16:22:17 +0100

    model with data augmentation (timestretching, pitchshifting) to train

--- CNN_topic/Convolution_1D/training_with_data_augmentation.py
+++ CNN_topic/Convolution_1D/training_with_data_augmentation.py
@@ -26,37 +26,19 @@ from time import time
 #     return augmented_audio
 
 
-def composer(audio_signal, sample_rate): 
-    pitch_shift_values = [-2, -1, 1, 2]
+def pitch_shifter(audio_signal, sample_rate): 
+    pitch_shift_values = [-2, 2]
     pitch_shift_transforms = [PitchShift(pitch_shift) for pitch_shift in pitch_shift_values]
     augmentations = Compose(pitch_shift_transforms)
     augmented_audio = augmentations(samples=audio_signal, sample_rate=sample_rate)
     return augmented_audio
 
-# def load_and_preprocess_data(df, target_length): 
-    data = []
-    labels = []
-
-    for index, row in tqdm(df.iterrows(), desc="Loading and preprocessing data", unit="file"):
-        audio, sr = librosa.load(row["relative_path"], sr=None)
-        if len(audio) < target_length:
-            audio = np.pad(audio, (0, target_length - len(audio)))
-        else: 
-            audio = audio[:target_length]
-
-        sos = signal.butter(6, [5000, 100000], 'bandpass', fs=sr, output='sos')
-        audio = signal.sosfiltfilt(sos, audio)
-
-        current_audio_label = row["label"]
-        augmented_audio = composer(audio, sr)
-
-        data.append(audio)
-        data.append(augmented_audio)
-        labels.append(current_audio_label)
-        labels.append(current_audio_label)
-
-    print("Doneeeeeeeeeeee")
-    return data, labels
+def time_stretcher(audio_signal, sample_rate): 
+    time_stretch_values = [0.8, 1.2]
+    time_stretch_transforms = [TimeStretch(time_stretch) for time_stretch in time_stretch_values]
+    augmentations = Compose(time_stretch_transforms)
+    augmented_audio = augmentations(samples=audio_signal, sample_rate=sample_rate)
+    return augmented_audio
 
 def load_and_preprocess_data(df, target_length): 
     data_list = []
@@ -72,11 +54,34 @@ def load_and_preprocess_data(df, target_length):
         sos = signal.butter(6, [5000, 100000], 'bandpass', fs=sr, output='sos')
         audio = signal.sosfiltfilt(sos, audio)
 
+
         current_audio_label = row["label"]
-        augmented_audio = composer(audio, sr)
-        
-        data_list.extend([audio, augmented_audio])
-        labels_list.extend([current_audio_label, current_audio_label])
+
+        original_audio = audio.astype(np.float32)
+        data_list.append(original_audio)
+        labels_list.append(current_audio_label)
+
+
+        apply_pitch_shift = np.random.rand() < 0.6
+        apply_time_stretching = np.random.rand() < 0.6
+
+        augmented_audio = None
+
+        if apply_pitch_shift and apply_time_stretching:
+            # Augmentation du fichier audio avec les deux process
+            augmented_audio = pitch_shifter(audio, sr)
+            augmented_audio = time_stretcher(augmented_audio, sr)
+        elif apply_pitch_shift:
+            # Augmentation du fichier audio avec le pitch shifting
+            augmented_audio = pitch_shifter(audio, sr)
+        elif apply_time_stretching:
+            # Augmentation du fichier audio avec le time stretching
+            augmented_audio = time_stretcher(audio, sr)
+
+        if augmented_audio is not None:
+            augmented_audio = augmented_audio.astype(np.float32)
+            data_list.append(augmented_audio)
+            labels_list.append(current_audio_label)
 
     data = np.array(data_list)
     labels = np.array(labels_list)
@@ -131,11 +136,11 @@ if __name__ == "__main__":
     #! ====== Set parameters ======
     conv1D_directory = Path.cwd() / "CNN_topic" / "Convolution_1D"
     test_directory = Path.cwd() / ".dataset" / "X_test"
-    models_directory = Path.cwd() / "CNN_topic" / "models"
-    model_name = "1d_cnn_l2_data_augmentation_pitch_shift.h5"
+    models_directory = Path.cwd() /  "../models"
+    model_name = "1d_cnn_l2_data_augmentation_pitch_shift_time_stretch_30_epochs.h5"
 
     # Set the path to the downloaded data
-    download_path = Path.cwd() / ".dataset"
+    download_path = Path.cwd() / "../../.dataset"
 
     # Audio parameters
     sample_rate = 256000
@@ -170,7 +175,7 @@ if __name__ == "__main__":
     print("Loading and preprocessing data")
     target_length = int(sample_rate * audio_duration_seconds)
 
-    X,y = load_and_preprocess_data(df, target_length)
+    X,y = load_and_preprocess_data(df[:50], target_length)
 
 
     print(X)
